6f8d7845adbb357a1d4a1eead0bc15382ee0e522
samuel Olowofela <samuel_olowofela@yahoo.com>
2015-02-12 22:44:58 +0000

    initial commit to live

---
+++ README.md
@@ -0,0 +1,3 @@
+## Cloudinary API Wrapper
+
+Cloudinary streamlines your entire image management pipeline - upload, storage, administration, manipulation and delivery.
---
+++ src/config/cloudinary.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Samuel
+ * Date: 12/02/2015
+ * Time: 22:17
+ */
+
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cloudinary API configuration
+    |--------------------------------------------------------------------------
+    |
+    | Before using Cloudinary you need to register and get some detail
+    | to fill in below, please visit cloudinary.com.
+    |
+    */
+
+    'cloudName'  => '',
+    'baseUrl'    => '',
+    'secureUrl'  => '',
+    'apiBaseUrl' => '',
+    'apiKey'     => '',
+    'apiSecret'  => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default image scaling to show.
+    |--------------------------------------------------------------------------
+    |
+    | If you not pass options parameter to Cloudy::show the default
+    | will be replaced.
+    |
+    */
+
+    'scaling'    => [
+        'format' => 'png',
+        'width'  => 150,
+        'height' => 150,
+        'crop'   => 'fit',
+        'effect' => null
+    ]
+
+];
